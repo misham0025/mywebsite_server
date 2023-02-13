e67eab3c19a61c6c234ec9d898ae18addf43029d
misham0025 <misham.john@kprinfo.com>
2023-02-13 15:13:20 +0530

    m

--- Server.js
+++ Server.js
@@ -1,38 +1,55 @@
-const express=require("express")
-const app=express()
-app.use(express.json())
-app.listen(process.env.PORT || 6826)
-console.log("working...")
-require('dotenv').config();
-const cors=require("cors")
-app.use(cors({origin:"http://localhost:3000"}))
-const mongodb=require("mongodb")
-const mongoClient=mongodb.MongoClient
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT || 6826);
+console.log("working...");
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors({ origin: "http://localhost:3000" }));
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
 
+app.get("/empDetails", cors(), async (req, res) => {
+  try {
+    const connection = await mongoClient.connect(process.env.MONGO_URL);
+    const db = connection.db("misham");
+    let response = await db.collection("movie").find().toArray();
+    await connection.close();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
 
+app.get("/productDetails", cors(), async (req, res) => {
+  try {
+    const connection = await mongoClient.connect(process.env.MONGO_URL);
+    const db = connection.db("misham");
+    let response = await db.collection("products").find().toArray();
+    await connection.close();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
 
-app.get("/empDetails",cors(),async(req,res)=>{
-    try {
-     const connection=await mongoClient.connect(process.env.MONGO_URL);
-     const db=connection.db("misham");
-    let response= await db.collection("movie").find().toArray()
-     await connection.close();
-     res.status(200).json(response)
- 
-    } catch (error) {
-     res.status(500).json({message:"error"})
-    }
- })
-
- app.get("/productDetails",cors(),async(req,res)=>{
-    try {
-     const connection=await mongoClient.connect(process.env.MONGO_URL);
-     const db=connection.db("misham");
-    let response= await db.collection("products").find().toArray()
-     await connection.close();
-     res.status(200).json(response)
- 
-    } catch (error) {
-     res.status(500).json({message:"error"})
-    }
- })
+app.get("/prodet", cors(), async (req, res) => {
+  try {
+    const pageSize =  10;
+    const page =  1;
+    const connection = await mongoClient.connect(process.env.MONGO_URL);
+    const db = connection.db("misham");
+    let response = await db.collection("products").find().toArray();
+    await connection.close();
+    const Model =response;
+    Model.find()
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)
+      .then(docs => {
+        res.send(docs);
+      })
+   
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
